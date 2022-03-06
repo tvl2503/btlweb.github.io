a480478276850472ed2f1c5e8e1fcf6844f493b2
Brian07 <hoanganh0594abc@gmail.com>
2022-03-06 16:03:35 +0700

    optimize

--- js/index.js
+++ js/index.js
@@ -1,32 +1,34 @@
 "use strict";
 import { insertStringElement, modelElement } from '../src/utils/element/utilsElement';
 
-let isActive = false;
-const navMenu = document.getElementById("nav-menu")
-const navToggle = document.getElementById('nav-toggle')
-const navClose = document.getElementById('nav-close');
+(function () {
+    let isActive = false;
+    const navMenu = document.getElementById("nav-menu")
+    const navToggle = document.getElementById('nav-toggle')
+    const navClose = document.getElementById('nav-close');
 
 
-const callback = (activeValue) => {
-    const model = document.querySelector('#navigation--model');
-    activeValue ? insertStringElement(document.body, modelElement('navigation--model')) : model.remove();
+    const callback = (activeValue) => {
+        const model = document.querySelector('#navigation--model');
+        activeValue ? insertStringElement(document.body, modelElement('navigation--model')) : model.remove();
 
-    const modelAfterInsert = document.querySelector('#navigation--model');
-    if (modelAfterInsert) {
-        modelAfterInsert.addEventListener('click', handleClick);
+        const modelAfterInsert = document.querySelector('#navigation--model');
+        if (modelAfterInsert) {
+            modelAfterInsert.addEventListener('click', handleClick);
+        }
     }
-}
-const handleClick = () => {
-    isActive = navMenu.classList.toggle('active');
-    callback(isActive)
-}
-navToggle.addEventListener('click', handleClick);
-navClose.addEventListener('click', handleClick);
+    const handleClick = () => {
+        isActive = navMenu.classList.toggle('active');
+        callback(isActive)
+    }
+    navToggle.addEventListener('click', handleClick);
+    navClose.addEventListener('click', handleClick);
 
-const navLink = document.querySelectorAll('.nav__link');
-function linkAction() {
-    const navMenu = document.getElementById('nav-menu');
-    navMenu.classList.remove('active');
-}
-navLink.forEach(n => n.addEventListener('click', linkAction));
+    const navLink = document.querySelectorAll('.nav__link');
+    function linkAction() {
+        const navMenu = document.getElementById('nav-menu');
+        navMenu.classList.remove('active');
+    }
+    navLink.forEach(n => n.addEventListener('click', linkAction));
+})();
 //# sourceMappingURL=index.js.map

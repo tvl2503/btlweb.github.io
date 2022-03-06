567470a18c0e75ca493da1592d9424417db26646
Brian07 <hoanganh0594abc@gmail.com>
2022-03-06 16:02:40 +0700

    add list item

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,56 +1,81 @@
 import { getHeader } from "../layout/header";
-import Swiper, {Navigation, Pagination} from 'swiper';
+import Swiper, { Navigation, Pagination } from "swiper";
+import { insertStringElement } from "../utils/element/utilsElement";
+import { classListToggleElement } from "../utils/classList";
 getHeader();
 
-const swiper = new Swiper('.swiper--container', {
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true
-    },
-    modules: [Pagination],
-    loop: true,
-    slidesPerView: 1,
-    autoplay: {
-        delay: 5000
-    },
-    speed: 750,
-})
+new Swiper(".swiper--container", {
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  modules: [Pagination],
+  loop: true,
+  slidesPerView: 1,
+  autoplay: {
+    delay: 5000,
+  },
+  speed: 750,
+});
 
-const swiperCollection = new Swiper('.swiper--collection', {
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
+new Swiper(".swiper--collection", {
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  modules: [Pagination, Navigation],
+  navigation: {
+    prevEl: ".swiper-button-prev",
+    nextEl: ".swiper-button-next",
+  },
+  autoplay: {
+    delay: 5000,
+  },
+  loop: true,
+  breakpoints: {
+    200: {
+      slidesPerView: "auto",
+      spaceBetween: 20,
     },
-    modules: [Pagination, Navigation],
-    navigation: {
-        prevEl: '.swiper-button-prev',
-        nextEl: '.swiper-button-next'
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 25,
     },
-    autoplay: {
-        delay: 5000
+    991: {
+      slidesPerView: 4,
+      spaceBetween: 30,
     },
-    loop: true,
-    breakpoints: {
-        // 200: {
-        //     slidesPerView: 1,
-        //     spaceBetween: 10
-        // },
-        // 576: {
-        //     slidesPerView: 2,
-        //     spaceBetween: 15
-        // },
-        200: {
-            slidesPerView: 'auto',
-            spaceBetween: 20
-        },
-        768: {
-            slidesPerView: 3,
-            spaceBetween: 25
-        },
-        991: {
-            slidesPerView: 4,
-            spaceBetween: 30
-        },
-    },
-    speed: 500
-})
+  },
+  speed: 500,
+});
+
+// fake from api
+
+(() => {
+  let options = ["Best Sellers", "New Arrivals", "On Trend"];
+  let defaultValue = "Best Sellers";
+  const textSelect = document?.querySelector(".text--select");
+
+  textSelect!.textContent = defaultValue;
+
+  const listOptions = document?.getElementById("list--options");
+  const button = document?.querySelector(".content--select");
+  const createList = options.map((item, index) => {
+    return `<li data-index=${index}>${item}</li>`;
+  });
+  const joinStringList = createList.join("");
+  insertStringElement(listOptions!, joinStringList, "afterbegin");
+  const handleListItem = () => {
+    classListToggleElement(listOptions!, "show");
+  };
+  button?.addEventListener("click", handleListItem);
+
+  const listItems = document?.querySelectorAll("li[data-index]");
+  if (listItems?.length > 0) {
+    listItems.forEach((item) => {
+      item.addEventListener("click", () => {
+        textSelect!.textContent = item.textContent;
+      });
+    });
+  }
+})();

e366744f1fec807e391fa899d8fc23a025b6c957
tvl2503 <volinhh1804@gmail.com>
2022-06-13 08:20:57 +0700

    idol tan

--- dist/constants/path.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ROOT_PATH = void 0;
-exports.ROOT_PATH = `${window.location.protocol}//${window.location.host}${window.location.pathname}`;
--- dist/utils/image.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.convertImageToURL = void 0;
-const convertImageToURL = (file) => {
-    return URL.createObjectURL(file);
-};
-exports.convertImageToURL = convertImageToURL;
